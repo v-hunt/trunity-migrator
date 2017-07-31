49ed001e4f78b379a9260ff28bd15fc3fae9eb9a
VicHunting <VicHunting@yandex.ua>
2017-08-01 00:55:26 +0300

    Add settings file

---
+++ trunity_migrator/settings.py
@@ -0,0 +1,40 @@
+#####################################################
+#                  BASIC SETTINGS
+# Attention!! The following settings must be filled
+# before running this script!
+#####################################################
+
+# set here your user name and password:
+USER_NAME = ""
+PASSWORD = ""
+
+# the name of Trunity book you need to import:
+TRUNITY_BOOK_NAME = ""
+
+CONTENT_TYPES = [  # All of these content types will be saved. You can comment out what you don't need:
+    "article",
+    # "questionpool",
+    # "exam"
+    # "news",
+    # "video",
+    # "podcast",
+    # "gallery",
+    # "game",
+    # "assignment",
+    # "whitepaper",
+    # "casestudy",
+    # "presentation",
+    # "lecture",
+    # "exercise",
+    # "teachingunit",
+]
+
+
+
+
+#-------- STRING CONSTANTS ----------------
+TRUNITY_2_API_ENDPOINT = "http://api.trunity.net/v1/"
+# TRUNITY_2_API_ENDPOINT = "http://api2.trunity.net/v1/"
+
+
+TRUNITY_2_APP_CODE = ''
