06a6f8fca81c1013ce4ed634b26f3aa61186475a
VicHunting <VicHunting@yandex.ua>
2017-08-06 20:06:49 +0300

    Implement fixer for table width

--- trunity_migrator/fixers.py
+++ trunity_migrator/fixers.py
@@ -1,9 +1,14 @@
+from typing import List
+
 from bs4 import BeautifulSoup
 
 from trunity_migrator.utils.static import url_repair
 
 
-BASE_FILE_URL = 'http://www.trunity.net'
+__all__ = (
+    'fix_img_src',
+    'fix_table_width',
+)
 
 
 def fix_img_src(html: str, base_url: str) -> str:
@@ -19,5 +24,20 @@ def fix_img_src(html: str, base_url: str) -> str:
     
     for img in soup.findAll('img'):
         img['src'] = url_repair(img['src'], base_url)
+
+    return soup.decode()
+
+
+def fix_table_width(html: str, old_widths: List[str], new_widths=List[str]):
+    if len(old_widths) != len(new_widths):
+        raise ValueError(
+            "old_widths and new_widths must have the same length!"
+        )
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    for old_width, new_width in zip(old_widths, new_widths):
+        for table_tag in soup.find_all('table', width=old_width):
+            table_tag['width'] = new_width
 
     return soup.decode()
--- trunity_migrator/tests/test_fixers.py
+++ trunity_migrator/tests/test_fixers.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from bs4 import BeautifulSoup
 
-from trunity_migrator.fixers import fix_img_src
+from trunity_migrator.fixers import *
 
 
 class FixersTestCase(TestCase):
@@ -32,3 +32,63 @@ class FixersTestCase(TestCase):
             BeautifulSoup(expected_html, "html.parser"),
             'html and expected_html are not equal!'
         )
+
+    def test_fix_table_width(self):
+        html = '''
+        <h2>Change width from 766 to 100%</h2>
+        <table border="0" cellpadding="0" cellspacing="0" height="36" style="background-color: rgb(191, 242, 255);" width="766">
+            <tbody>
+                <tr>
+                    <td style="width: 712px;"><strong>The structure of DNA</strong></td>
+                    <td style="width: 20px;">&nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <h2>Change width from 770 to 70%</h2>
+        <table border="0" cellpadding="0" cellspacing="0" height="36" style="background-color: rgb(191, 242, 255);" width="770">
+            <tbody>
+                <tr>
+                    <td style="width: 712px;"><strong>The structure of DNA</strong></td>
+                    <td style="width: 20px;">&nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+        '''
+
+        expected_html = '''
+        <h2>Change width from 766 to 100%</h2>
+        <table border="0" cellpadding="0" cellspacing="0" height="36" style="background-color: rgb(191, 242, 255);" width="100%">
+            <tbody>
+                <tr>
+                    <td style="width: 712px;"><strong>The structure of DNA</strong></td>
+                    <td style="width: 20px;">&nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2>Change width from 770 to 70%</h2>
+        <table border="0" cellpadding="0" cellspacing="0" height="36" style="background-color: rgb(191, 242, 255);" width="70%">
+            <tbody>
+                <tr>
+                    <td style="width: 712px;"><strong>The structure of DNA</strong></td>
+                    <td style="width: 20px;">&nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+        '''
+
+        result_html = fix_table_width(
+            html,
+            old_widths=["766", "770"],
+            new_widths=["100%", "70%"]
+        )
+
+        self.assertEqual(
+            BeautifulSoup(expected_html, "html.parser"),
+            BeautifulSoup(result_html, "html.parser"),
+            "Result html is not equal to expected!"
+        )
+
+        # TODO: try use this: from bs4.testing import SoupTest
+        # self.assertSoupEquals
