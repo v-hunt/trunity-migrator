7a6759b713aa938391a11d0c20cc5da622188b97
VicHunting <VicHunting@yandex.ua>
2017-08-07 23:55:16 +0300

    Implement basic CLI

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='trunity_migrator',
-    version='0.1',
+    version='0.2.2',
     packages=find_packages(),
+    scripts=['trunity_migrator/bin/trunity-migrator'],
     install_requires=[
         'requests',
         'beautifulsoup4',
