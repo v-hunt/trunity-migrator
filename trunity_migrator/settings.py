b436b9711ae6e184fa05c79fc08bd44557c8a60c
VicHunting <VicHunting@yandex.ua>
2017-08-07 23:51:09 +0300

    Extand example settings

--- trunity_migrator/settings.py
+++ trunity_migrator/settings.py
@@ -5,20 +5,17 @@
 #####################################################
 
 # set here your user name and password:
-TRUNITY_2_LOGIN = ""
-TRUNITY_2_PASSWORD = ""
+TRUNITY_2_LOGIN = "my-trunity-2-login"
+TRUNITY_2_PASSWORD = "secret"
 
-TRUNITY_3_LOGIN = ""
-TRUNITY_3_PASSWORD = ""
+TRUNITY_3_LOGIN = "my-trunity-3-login"
+TRUNITY_3_PASSWORD = "verysecret"
 
 # the name of Trunity book you need to import:
 TRUNITY_2_BOOK_NAME = "Integrating Concepts in Biology"
-TRUNITY_3_BOOK_NAME = "Integrating Concepts in Biology v11"
+TRUNITY_3_BOOK_NAME = "Integrating Concepts in Biology v13"
 
-# this will be added to all 'src' urls
-STATIC_URL = 'http://www.trunity.net'
-
-# All of these content types will be saved.
+# All of these content types will be imported.
 # You may comment out what you don't need:
 CONTENT_TYPES = [
     "article",
@@ -41,15 +38,19 @@ CONTENT_TYPES = [
 
 
 
-#-------- STRING CONSTANTS ----------------
 TRUNITY_2_API_ENDPOINT = "http://api.trunity.net/v1/"
 # TRUNITY_2_API_ENDPOINT = "http://api2.trunity.net/v1/"
 
 
-TRUNITY_2_APP_CODE = ''
+TRUNITY_2_APP_CODE = 'your-app-code'
 
 
-# -------------- Fixers -----------------
+###############################################################################
+#                           FIXERS SETTINGS
+###############################################################################
 
+
+# this will be added to all 'src' urls
+STATIC_URL = 'http://www.trunity.net'
 IMG_BASE_URL = 'http://www.trunity.net'
 
