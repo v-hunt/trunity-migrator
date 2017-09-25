450d075bae35a6a0e2b194a1a92bc4b31ec58f3a
VicHunting <VicHunting@yandex.ua>
2017-09-26 02:02:49 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trunity_migrator',
-    version='0.4.3',
+    version='0.4.4',
     packages=find_packages(),
     scripts=['trunity_migrator/bin/trunity-migrator'],
     install_requires=[
