6455d02b4f3825cb377b8003424ff13948900985
VicHunting <VicHunting@yandex.ua>
2017-10-25 03:27:41 +0300

    Add converters for answers

---
+++ trunity_migrator/questionnaires/answer_converters.py
@@ -0,0 +1,64 @@
+"""
+Collection of converters that transform T2 answers to list of T3 answer objects.
+"""
+from typing import List
+
+from trunity_3_client.builders import Answer, TrueFalseAnswer
+
+
+DEFAULT_SCORE = 1
+
+
+def convert_multiple_choice(t2_question: dict) -> List[Answer]:
+    correct_answer_index = int(t2_question['correct'])
+    feedback = t2_question['feedback']
+
+    result = []
+    for index, answer in enumerate(t2_question['answers']):
+        correct = True if correct_answer_index == index else False
+        score = DEFAULT_SCORE if correct else 0
+
+        result.append(
+            Answer(answer, correct, score=score, feedback=feedback)
+        )
+
+    return result
+
+
+def convert_multiple_answer(t2_question: dict) -> List[Answer]:
+    correct_answer_indexes = map(int, t2_question['correct'])
+    feedback = t2_question['feedback']
+
+    result = []
+    for index, answer in enumerate(t2_question['answers']):
+        correct = True if index in correct_answer_indexes else False
+        score = DEFAULT_SCORE if correct else 0
+
+        result.append(
+            Answer(answer, correct, score=score, feedback=feedback)
+        )
+
+    return result
+
+
+def convert_true_false(t2_question: dict) -> List[TrueFalseAnswer]:
+    feedback = t2_question['feedback']
+    correct = True if t2_question['correct'] == 'true' else False
+
+    def get_score(x: bool):
+        return DEFAULT_SCORE if x else 0
+
+    return [
+        TrueFalseAnswer(correct, True, score=get_score(correct), feedback=feedback),
+        TrueFalseAnswer(not correct, False, score=get_score(not correct), feedback=feedback),
+    ]
+
+
+def convert_short_answer(t2_question: dict) -> List[Answer]:
+    feedback = t2_question['feedback']
+
+    return [
+        Answer(answer, True, score=DEFAULT_SCORE, feedback=feedback)
+        for answer in t2_question['answers']
+    ]
+
---
+++ trunity_migrator/questionnaires/tests/test_answer_converters.py
@@ -0,0 +1,95 @@
+from unittest import TestCase
+
+from trunity_3_client.builders import Answer, NumericAnswer, TrueFalseAnswer
+
+from trunity_migrator.questionnaires.answer_converters import (
+    convert_multiple_choice,
+    convert_multiple_answer,
+    convert_true_false,
+    convert_short_answer,
+)
+
+
+class ConvertersTestCase(TestCase):
+
+    def test_convert_multiple_choice(self):
+        t2_question = {
+            'type': 'multipleChoice',
+            'feedback': '',
+            'answers': ['foo', 'bar', 'baz'],
+            'correct': '0',
+            'question': '<p>This is a Multiple Choice</p>\n',
+            'questionId': 'qceacacd180549eb663a3f3b8b75fdf4f'
+        }
+
+        t3_expected_answers = [
+            Answer('foo', True, 1),
+            Answer('bar', False, 0),
+            Answer('baz', False, 0),
+        ]
+
+        self.assertListEqual(
+            convert_multiple_choice(t2_question),
+            t3_expected_answers
+        )
+
+    def test_convert_multiple_answer(self):
+        t2_question = {
+            'type': 'multipleAnswer',
+            'feedback': '',
+            'answers': ['foo', 'bar', 'baz'],
+            'correct': ['0', '1'],
+            'question': '<p>This is Multiple Answer</p>\n',
+            'questionId': 'qe7c13ce3d94a37e84669953d44425ab1'
+        }
+
+        t3_expected_answers = [
+            Answer('foo', True, 1),
+            Answer('bar', True, 1),
+            Answer('baz', False, 0),
+        ]
+
+        self.assertListEqual(
+            convert_multiple_answer(t2_question),
+            t3_expected_answers
+        )
+
+    def test_convert_true_false(self):
+        t2_question = {
+            'type': 'trueFalse',
+            'question': '<p>This is TrueFalse</p>\n',
+            'feedback': '',
+            'correct': 'true',
+            'questionId': 'q029c06026471ad680217745405038d5d'
+        }
+
+        t3_expected_answers = [
+            TrueFalseAnswer(True, True, 1),
+            TrueFalseAnswer(False, False, 0),
+        ]
+
+        self.assertListEqual(
+            convert_true_false(t2_question),
+            t3_expected_answers
+        )
+
+    def test_convert_short_answer(self):
+        t2_question = {
+            'type': 'shortAnswer',
+            'question': '<p>This is Short Answer</p>\n',
+            'answers': ['foo', 'bar', '42'],
+            'feedback': '',
+            'questionId': 'qe5dc29499b1664e1581c240379fd3101'
+        }
+
+        t3_expected_answers = [
+            Answer('foo', True, 1),
+            Answer('bar', True, 1),
+            Answer('42', True, 1),
+        ]
+
+        self.assertListEqual(
+            convert_short_answer(t2_question),
+            t3_expected_answers
+        )
+
